90e2612d772ec8d686280cc86646b7e7d401637a
Lucas M. T. Friedrich <frielucas@gmail.com>
2023-11-21 14:09:19 -0300

    Criação de um campo selecionavel para o status

---
+++ src/components/courts/entradaSelect.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+interface EntradaSelectProps {
+  texto: string;
+  valor: string | string[]; // Aceita uma única string ou um array de strings
+  opcoes: string[]; // Opções disponíveis
+  onChange?: (valor: string | string[]) => void;
+}
+
+export default function EntradaSelect(props: EntradaSelectProps) {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setDropdownVisible(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col mt-3 relative">
+      <label className="mb-2"> {props.texto} </label>
+      <div className="relative inline-block text-left">
+        <div>
+          <span
+            onClick={() => setDropdownVisible(!dropdownVisible)}
+            className={`
+              cursor-pointer border border-indigo-500 rounded-l g bg-gray-100 px-4 py-2
+              focus:outline-none focus:bg-white
+            `}
+          >
+            {Array.isArray(props.valor)
+              ? props.valor.join(', ') || 'Selecione...'
+              : props.valor || 'Selecione...'}
+          </span>
+        </div>
+        {dropdownVisible && (
+          <div
+            ref={dropdownRef}
+            className="origin-top-left absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+          >
+            <div className="py-1">
+              {props.opcoes.map((opcao) => (
+                <div
+                  key={opcao}
+                  onClick={() => {
+                    props.onChange?.(opcao);
+                    setDropdownVisible(false);
+                  }}
+                  className={`
+                    cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900
+                  `}
+                >
+                  {opcao}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/components/courts/formulario.tsx
+++ src/components/courts/formulario.tsx
@@ -2,6 +2,7 @@ import Entrada from "./entrada"
 import Court from "@/core/Court"
 import { useState } from "react";
 import Button from "./button";
+import EntradaSelect from "./entradaSelect";
 
 interface FormularioProps {
     court: Court
@@ -10,29 +11,57 @@ interface FormularioProps {
 }
 
 export default function Formulario(
-    props: FormularioProps){
-        const id = props.court?.id
-        const [description, setDescription] = useState(props.court?.description)
-        const [status, setStatus] = useState(props.court?.status)
+    props: FormularioProps
+  ) {
+    const id = props.court?.id;
+    const [description, setDescription] = useState(props.court?.description);
+    const [status, setStatus] = useState<string[] | string>(
+      Array.isArray(props.court?.status) ? props.court?.status : [props.court?.status || ""]
+    );
+    const opcoes = ["AVAILABLE", "MAINTENANCE", "BROKEN"];
 
-
-
-        return(
-            <div>
-                {id ? (<Entrada texto="id" valor={id} somenteLeitura ></Entrada>) : false}
-                <Entrada texto="Description" valor={description} onChange={setDescription}></Entrada>
-                <Entrada texto="Status" valor={status} onChange={setStatus}></Entrada>
-                <div className="flex justify-end mt-5" >
-                    <Button className="mr-3" cor="bg-gradient-to-r from-blue-500 to-blue-700"
-                        onClick={() => props.courtMudou?.(new Court(
-                            id, description, status))}>
-
-                            {id ? 'Alterar' : 'Salvar'}
-                    </Button>
-                    <Button cor="bg-gradient-to-r from-gray-500 to-gray-700"
-                        onClick={props.cancelado}> Cancelar
-                    </Button>
-                </div>
-            </div>
-        )
-    }
+  
+    return (
+      <div>
+        {id ? (
+          <Entrada texto="id" valor={id} somenteLeitura></Entrada>
+        ) : null}
+        <Entrada
+          texto="Description"
+          valor={description}
+          onChange={setDescription}
+        ></Entrada>
+  
+        <EntradaSelect
+            texto="Status"
+            valor={status}
+            opcoes={opcoes} // Certifique-se de passar a propriedade opcoes
+            onChange={(newStatus) => setStatus(newStatus)}
+            />
+  
+        <div className="flex justify-end mt-5">
+          <Button
+            className="mr-3"
+            cor="bg-gradient-to-r from-blue-500 to-blue-700"
+            onClick={() =>
+              props.courtMudou?.(
+                new Court(
+                  id,
+                  description,
+                  status.toString()
+                )
+              )
+            }
+          >
+            {id ? "Alterar" : "Salvar"}
+          </Button>
+          <Button
+            cor="bg-gradient-to-r from-gray-500 to-gray-700"
+            onClick={props.cancelado}
+          >
+            Cancelar
+          </Button>
+        </div>
+      </div>
+    );
+  }
