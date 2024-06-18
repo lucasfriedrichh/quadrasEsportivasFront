95c33175ac157bd32bfe423b3d8b9ee48cacc225
Lucas M. T. Friedrich <frielucas@gmail.com>
2024-06-18 09:50:57 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
-    return (
-      <div>
-        <h2>Registros de quadras Esportivas</h2>
-      </div>
-    )
-  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column', 
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        background: 'linear-gradient(to bottom left, #3B82F6, #7C3AED, #3B82F6)',
+        color: 'white', 
+        textAlign: 'center' 
+      }}
+    >
+      <h2 style={{ fontSize: '3rem', fontFamily: 'Arial, sans-serif' }}>
+        Registros de quadras Esportivas
+      </h2>
+      <br />
+      <p>By Lucas Friedrich and Leonardo Salinet</p>
+    </div>
+  );
+}
+
